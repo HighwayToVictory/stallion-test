99176332fc193a983e72f6bed3e1aeac401a8d1c
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-20 11:33:11 +0330

    add: helpers

---
+++ src/helpers/fetch-wrapper.js
@@ -0,0 +1,56 @@
+import { useAuthStore } from '@/stores';
+
+export const fetchWrapper = {
+    get: request('GET'),
+    post: request('POST'),
+    put: request('PUT'),
+    delete: request('DELETE')
+};
+
+function request(method) {
+    return (url, body) => {
+        const requestOptions = {
+            method,
+            headers: authHeader(url)
+        };
+        if (body) {
+            requestOptions.headers['Content-Type'] = 'application/json';
+            requestOptions.body = JSON.stringify(body);
+        }
+        return fetch(url, requestOptions).then(handleResponse);
+    }
+}
+
+// helper functions
+
+function authHeader(url) {
+    const { token } = useAuthStore();
+
+    const isLoggedIn = !!token;
+    const isApiUrl = url.startsWith(import.meta.env.VITE_API_URL);
+
+    if (isLoggedIn && isApiUrl) {
+        return { Authorization: `Bearer ${token}` };
+    } else {
+        return {};
+    }
+}
+
+async function handleResponse(response) {
+    const isJson = response.headers?.get('content-type')?.includes('application/json');
+    const data = isJson ? await response.json() : null;
+
+    // check for error response
+    if (!response.ok) {
+        const { user, logout } = useAuthStore();
+        if ([401, 403].includes(response.status) && user) {
+            logout();
+        }
+
+        // get error message from body or default to response status
+        const error = (data && data.message) || response.status;
+        return Promise.reject(error);
+    }
+
+    return data;
+}
---
+++ src/stores/auth.store.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+
+import { fetchWrapper } from '@/helpers';
+import { router } from '@/router';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}/login`;
+
+export const useAuthStore = defineStore({
+    id: 'auth',
+    state: () => ({
+        token: JSON.parse(localStorage.getItem('jwt')),
+        returnUrl: null
+    }),
+    actions: {
+        async login(username, password) {
+            const token = await fetchWrapper.post(`${baseUrl}/authenticate`, { username, password });    
+
+            this.token = token;
+
+            localStorage.setItem('jwt', token);
+
+            router.push(this.returnUrl || '/');
+        },
+        logout() {
+            this.user = null;
+
+            localStorage.removeItem('jwt');
+
+            router.push('/login');
+        }
+    }
+});
