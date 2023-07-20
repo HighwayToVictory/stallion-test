a30f2a6ca4f1809b648f8b771f5e99ef6d13b40a
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-20 11:14:53 +0330

    fix: vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '../views/HomeView/index.vue'
+import LoginView from '../views/LoginView/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView/index.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    next({
+      path: '/login',
+      params: { nextUrl: to.fullPath }
+    })
+  } else {
+    next()
+  }
 })
 
 export default router
