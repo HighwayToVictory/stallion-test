e9529a49fcc6344b78f31e37a9486d4a4f40dda7
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-20 11:41:55 +0330

    fix: auth display

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Navigation from './components/Navigation.vue'
+import { useAuthStore } from '@/stores'
+
+const authStore = useAuthStore();
 </script>
 
 <template>
   <div class="container p-0 m-0">
     <div class="row p-0 m-0" style="min-height: -webkit-fill-available;">
       <div class="col-2 p-0 m-0" style="min-height: -webkit-fill-available;">
-        <Navigation />
+        <Navigation v-if="authStore.token" />
       </div>
       <div class="col-10" style="min-height: -webkit-fill-available;">
         <RouterView />
