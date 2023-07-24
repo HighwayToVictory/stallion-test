ef91f3be2467dfe45e9e879245a30efafd11499d
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-24 09:22:45 +0330

    fix: namespaces main page bug

--- src/views/NamespacesView/index.vue
+++ src/views/NamespacesView/index.vue
@@ -34,15 +34,13 @@ import { parser } from '@/utils';
             {{ parser.parseDate(item['created_at']) }}
           </td>
           <td>
-            <button v-on:click="deleteNamespace(item['id'])" class="btn btn-danger text-left">
+            <button v-on:click="deleteNamespace(item['id'])" class="btn btn-danger text-left" style="margin-right: 5px;">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-x-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
               </svg>
               Delete
             </button>
-          </td>
-          <td>
-            <RouterLink :to="`/namespaces/edit/${item['id']}`" class="btn btn-danger text-left">
+            <RouterLink :to="`/namespaces/edit/${item['id']}`" class="btn btn-primary text-left">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-x-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
               </svg>
@@ -56,6 +54,8 @@ import { parser } from '@/utils';
 </template>
 
 <script>
+import { namespacesApi } from '@/api';
+
 export default {
   data() {
     return {
@@ -63,9 +63,13 @@ export default {
     }
   },
   methods: {
-    deleteNamespace(id) {
-
+    async deleteNamespace(id) {
+      await namespacesApi.remove(id);
+      this.namespaces = await namespacesApi.get();
     }
+  },
+  async mounted() {
+    this.namespaces = await namespacesApi.get();
   }
 }
 </script>
