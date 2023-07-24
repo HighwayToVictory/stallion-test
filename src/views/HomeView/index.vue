35d8050ca54541f35af396b7fe482507518002de
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-24 12:51:58 +0330

    style: restyle dashboard

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -7,24 +7,24 @@ import { parser } from '@/utils';
     <div class="h1 border-bottom pb-2 mb-5">
       Welcome `{{ user }}`
     </div>
-    <div class="mb-5">
-      <div class="d-flex justify-content-between align-items-center mb-4" v-for="item in namespaces" :key="item.id">
-        <span class="badge bg-secondary" v-on:click="select(item)">
-          {{ item.name }}
-        </span>
-        <span>
-          <span class="badge bg-primary badge-pill" style="margin-right: 10px;">
-            {{ parser.parseDate(item.created_at) }}
-          </span>
-          <span class="badge" :class="this.namespace.id === item.id ? 'bg-success'  : 'bg-danger'">
-            {{ this.namespace.id === item.id ? 'selected'  : 'select' }}
-          </span>
-        </span>
-      </div>
+    <div class="mb-5" style="text-align: justify;">
+      This is the main dashboard. You can select a namespace from available namespaces for you
+      and see the projects of that namespace. If you cannot see a namespace in your list, contact
+      to one of the admin users so that they add you to that namespace. You cannot see neither namespaces
+      that you are not belong to or their projects.
     </div>
     <div v-if="this.namespace != null">
-      <div class="h3 mb-5 border-bottom pb-2">
-        Projects
+      <div class="mb-5 border-bottom pb-2 row">
+        <div class="h3 col-10">
+          Projects
+        </div>
+        <div class="col-2">
+          <select v-model="namespace" class="form-select" aria-label=".form-select-sm example">
+            <option v-for="item in namespaces" :key="item.id" :value="item">
+              {{ item.name }}
+            </option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
