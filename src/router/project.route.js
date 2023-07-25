1119d655cf5754c0ddd1bb3624633bc8c98bd69d
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-25 10:36:46 +0330

    add: project view page

--- src/router/project.route.js
+++ src/router/project.route.js
@@ -1,9 +1,14 @@
 import CreateProjectView from '../views/ProjectView/views/CreateProjectView.vue'
+import ProjectView from '../views/ProjectView/index.vue'
 
 
 export default [
     {
         path: '/projects/new',
         component: CreateProjectView,
+    },
+    {
+        path: '/projects/:namespace/:id',
+        component: ProjectView
     }
 ];
---
+++ src/views/ProjectView/index.vue
@@ -0,0 +1,26 @@
+<template>
+    {{ namespace_id }} | {{ project_id }}
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+export default {
+    data() {
+        return {
+            namespace_id: 0,
+            project_id: 0
+        }
+    },
+    mounted() {
+        const route = useRoute();
+
+        this.namespace_id = route.params.namespace;
+        this.project_id = route.params.id;
+    }
+}
+</script>
+
+<style>
+
+</style>
