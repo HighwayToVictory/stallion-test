8ae827b7fcd4f40e4ea2c833d60f863faa5fe0ad
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-25 10:49:45 +0330

    bug: in project response

--- src/views/ProjectView/index.vue
+++ src/views/ProjectView/index.vue
@@ -1,22 +1,51 @@
 <template>
-    {{ namespace_id }} | {{ project_id }}
+    <div class="p-5">
+        <div class="h1 border-bottom pb-2 mb-5">
+            Project `{{ project.name }}`
+        </div>
+        <div>
+            <div class="h4">
+                Host: {{ project.host }}
+            </div>
+            <div>
+                Created By `{{ project.creator }}` at `{{ project.created_at }}`
+            </div>
+            <div>
+                About: {{ project.description }}
+            </div>
+            <div v-if="project.labels">
+                <div class="h5">
+                    Labels
+                </div>
+                <span v-for="item in project.labels" :key="item.key">
+                    {{ item.key + "=" + item.value }}
+                </span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
+import { useRoute } from 'vue-router';
+import { projectsApi } from '@/api';
 
 export default {
     data() {
         return {
             namespace_id: 0,
-            project_id: 0
+            project_id: 0,
+            project: ""
         }
     },
-    mounted() {
+    async mounted() {
         const route = useRoute();
 
         this.namespace_id = route.params.namespace;
         this.project_id = route.params.id;
+
+        this.project = await projectsApi.getSingle(this.namespace_id, this.project_id);
+
+        console.log(this.project.description);
     }
 }
 </script>
