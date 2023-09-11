17c85160f5971533b4b4b6040ef669a639db9b47
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-11 13:14:28 +0330

    init: cluter metrics page

--- src/views/MetricsView/views/Metrics.vue
+++ src/views/MetricsView/views/Metrics.vue
@@ -1,7 +1,17 @@
 <template>
-  <h1>
-    Metrics
-  </h1>
+  <div class="p-5">
+    <div class="h3 pb-2 mb-3">
+      Metrics
+    </div>
+    <div class="bg-light rounded p-3">
+      <div style="text-align: justify;">
+        In metrics page you can view cluster metrics to get some information about
+        the projects, namespaces, users and their activities. In here you can check
+        system health and other components activities like mysql database, core api, ftp server.
+        This page <span style="color: darkred;">does not</span> provide actions to change cluster settings.
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
