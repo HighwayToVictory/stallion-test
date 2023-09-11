153fd8eaf28831837d2b193bd49f681f869bdfdb
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-11 12:58:37 +0330

    fix: dashboard sort bug

--- src/views/HomeView/views/Dashboard.vue
+++ src/views/HomeView/views/Dashboard.vue
@@ -53,7 +53,7 @@ const authStore = useAuthStore();
             namespace projects
           </div>
           <div>
-            <button v-on:click="compare('date', false)" class="btn btn-sm btn-dark" style="margin-right: 10px;">
+            <button v-on:click="compare('date')" class="btn btn-sm btn-dark" style="margin-right: 10px;">
               <svg v-if="sortStatus.get('date') == 'down'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down me-2" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
               </svg>
@@ -62,7 +62,7 @@ const authStore = useAuthStore();
               </svg>
               Sort by date
             </button>
-            <button v-on:click="compare('name', false)" class="btn btn-sm btn-dark">
+            <button v-on:click="compare('name')" class="btn btn-sm btn-dark">
               <svg v-if="sortStatus.get('name') == 'down'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down me-2" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
               </svg>
@@ -163,15 +163,19 @@ export default {
         const av = a[field];
         const bv = b[field];
 
-        if (reverse) {
-          return av < bv ? 1 : -1;
+        if (typeof av == "string") {
+          return reverse ? av.localeCompare(bv) : bv.localeCompare(av);
         } else {
-          return av > bv ? 1 : -1;
+          if (reverse) {
+            return av < bv ? 1 : -1;
+          } else {
+            return av > bv ? 1 : -1;
+          }
         }
       }
     },
-    compare(field, reverse) {
-      this.projects.sort(this.compareObj(field, reverse));
+    compare(field) {
+      this.projects.sort(this.compareObj(field, this.sortStatus.get(field) === "up"));
       this.sortStatus.set(field, this.sortStatus.get(field) === "up" ? "down" : "up");
     }
   },
