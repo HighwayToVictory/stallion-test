1d09733073fa5b2f77fd6dce449b701061e081a3
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-03 13:00:47 +0330

    update: api index

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,6 @@
 // backend service
 
 export * from './admin.users.api';
-export * from './admin.namespaces.api';
-export * from './namespaces.api';
 export * from './user.projects.api';
 export * from './projects.api';
 export * from './user.api';
