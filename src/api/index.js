248b7677ed89223e9692f4176856231c031dcc3a
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-03 13:08:14 +0330

    update: api index

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,6 @@
 // api module is used to make http calls to our
 // backend service
 
-export * from './admin.users.api';
-export * from './user.projects.api';
 export * from './projects.api';
-export * from './user.api';
+export * from './users.api';
 export * from './metrics.api';
