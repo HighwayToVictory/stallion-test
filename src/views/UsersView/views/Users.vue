3f77f78bdd8b1b4dbdae8276cc655ff6e77a9152
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-04 10:27:42 +0330

    fix: users actions

--- src/views/UsersView/views/Users.vue
+++ src/views/UsersView/views/Users.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { RouterLink } from 'vue-router'
 import { enumUtils, parser } from '@/utils';
 </script>
 
@@ -37,13 +36,13 @@ import { enumUtils, parser } from '@/utils';
           Password
         </label>
       </div>
-        <RouterLink style="inline-size: none;: 100%;" to="/users/new" type="button" class="btn btn-primary">
+        <button style="inline-size: none;: 100%;" type="button" class="btn btn-primary" v-on:click="create()">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-fill-add" viewBox="0 0 16 16">
             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
             <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>
           </svg>
           new user
-        </RouterLink>
+        </button>
       </div>
     </div>
     <div class="bg-light rounded p-3 g-2">
@@ -100,6 +99,16 @@ export default {
 
       const alertStore = useAlertStore();
       alertStore.success(`User deleted!`);
+    },
+    async create() {
+      await usersApi.create(this.username, this.password);
+      this.users = await usersApi.get();
+
+      const alertStore = useAlertStore();
+      alertStore.success(`User ${this.username} created!`);
+
+      this.username = "";
+      this.password = "";
     }
   },
   async mounted() {
