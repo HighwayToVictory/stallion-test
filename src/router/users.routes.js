5ff06d9f617181ec1ac0e5593491a6f4a6631f43
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-04 09:27:23 +0330

    update: bugs

--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -1,18 +1,10 @@
-import { Users, Create } from '@/views/UsersView'
+import { Users } from '@/views/UsersView'
 
 
 export default [
     {
         path: '/users',
         component: Users,
-        meta: {
-            requiresAuth: true,
-            adminRoute: true
-        }
-    },
-    {
-        path: '/users/new',
-        component: Create,
         meta: {
             requiresAuth: true,
             adminRoute: true
--- src/views/UsersView/index.js
+++ src/views/UsersView/index.js
@@ -1,4 +1,1 @@
 export { default as Users } from './views/Users.vue';
-export { default as Profile } from './views/Profile.vue';
-export { default as Create } from './views/Create.vue';
-export { default as Edit } from './views/Edit.vue';
