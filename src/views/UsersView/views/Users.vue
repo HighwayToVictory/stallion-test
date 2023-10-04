0e4f07edf7035c0d3b685b2ae84cae13f054a069
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-04 09:22:43 +0330

    fix: bugs

--- src/views/UsersView/views/Users.vue
+++ src/views/UsersView/views/Users.vue
@@ -19,7 +19,7 @@ import { enumUtils, parser } from '@/utils';
         <div class="mb-4">
           Create new users in order to give them suitable rules to work with application.
         </div>
-        <RouterLink style="width: 100%;" to="/users/new" type="button" class="btn btn-primary">
+        <RouterLink style="inline-size: none;: 100%;" to="/users/new" type="button" class="btn btn-primary">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-fill-add" viewBox="0 0 16 16">
             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
             <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>
@@ -53,7 +53,7 @@ import { enumUtils, parser } from '@/utils';
               {{ parser.parseTime(user['created_at']) }}
             </td>
             <td style="text-align: center;">
-              <button v-on:click="deleteUser(user['id'])" class="btn btn-sm btn-danger text-left" style="margin-right: 5px;">
+              <button v-on:click="deleteUser(user['id'])" class="btn btn-sm btn-danger text-left" style="margin-inline-end: 5px;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-x-fill" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
                 </svg>
