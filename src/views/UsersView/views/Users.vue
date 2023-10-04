1062e09e81f3a8c453bb65222225bea64cc63141
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-04 10:25:13 +0330

    update: users page

--- src/views/UsersView/views/Users.vue
+++ src/views/UsersView/views/Users.vue
@@ -4,21 +4,39 @@ import { enumUtils, parser } from '@/utils';
 </script>
 
 <template>
-  <div class="p-5">
+  <div class="p-5 rounded shadow m-5">
     <div class="h3 pb-2">
       Users
     </div>
     <div class="row p-0 m-0 mb-3" style="grid-column-gap: 20px;">
       <div class="col bg-light rounded p-3" style="text-align: justify;">
         In users page you can see all of the system users. You can create new users
-        or remove the existing ones. Pay attention to user roles. Only admin users have
-        access to users, namespaces, and instructions pages. Developer users can create projects
-        and execute them.
+        or remove the existing ones. Since all users can have access to every component
+        and every action, make sure to authorize your clients before giving them access
+        to the application.
       </div>
       <div class="col bg-light rounded p-3">
         <div class="mb-4">
           Create new users in order to give them suitable rules to work with application.
         </div>
+        <div class="form-outline mb-4">
+        <input v-model="username" type="text" id="username" class="form-control" placeholder="admin ..." />
+        <label class="form-label" for="username">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi me-2 bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+          </svg>
+          Username
+        </label>
+      </div>
+      <div class="form-outline mb-4">
+        <input v-model="password" type="password" id="password" class="form-control" placeholder="password ..." />
+        <label class="form-label" for="password">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi me-2 bi-key-fill" viewBox="0 0 16 16">
+            <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+          </svg>
+          Password
+        </label>
+      </div>
         <RouterLink style="inline-size: none;: 100%;" to="/users/new" type="button" class="btn btn-primary">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-fill-add" viewBox="0 0 16 16">
             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
@@ -36,7 +54,6 @@ import { enumUtils, parser } from '@/utils';
         <thead>
           <tr>
             <th scope="col" style="text-align: center;">User</th>
-            <th scope="col" style="text-align: center;">Role</th>
             <th scope="col" style="text-align: center;">Created at</th>
             <th scope="col" style="text-align: center;">Actions</th>
           </tr>
@@ -47,24 +64,15 @@ import { enumUtils, parser } from '@/utils';
               {{ user['username'] }}
             </td>
             <td style="text-align: center;">
-              {{ enumUtils.role(user['role']) }}
-            </td>
-            <td style="text-align: center;">
               {{ parser.parseTime(user['created_at']) }}
             </td>
             <td style="text-align: center;">
-              <button v-on:click="deleteUser(user['id'])" class="btn btn-sm btn-danger text-left" style="margin-inline-end: 5px;">
+              <button v-on:click="deleteUser(user['id'])" class="btn btn-sm btn-danger text-left">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi me-2 bi-person-x-fill" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
                 </svg>
                 delete user
               </button>
-              <RouterLink :to="'/users/edit/'+user['id']" class="btn btn-sm btn-primary text-left">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil me-2" viewBox="0 0 16 16">
-                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                </svg>
-                edit user
-              </RouterLink>
             </td>
           </tr>
         </tbody>
@@ -80,6 +88,8 @@ import { useAlertStore } from '@/stores';
 export default {
   data() {
     return {
+      username: "",
+      password: "",
       users: []
     }
   },
