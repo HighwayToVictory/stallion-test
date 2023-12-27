be75dfc737059f8bddc32e8008125d45c1ade03c
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-27 13:07:03 +0330

    add: live api

---
+++ src/api/live.api.js
@@ -0,0 +1,16 @@
+import { fetchWrapper } from '@/helpers';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}/live-tracking`;
+
+
+// projectsApi manages the projects endpoints
+export const projectsApi = {
+    get: get(),
+};
+
+// get live tracking of project
+function get() {
+    return async (project_id, id) => {
+        return fetchWrapper.get(`${baseUrl}/${project_id}?id=${id}`);
+    }
+}
