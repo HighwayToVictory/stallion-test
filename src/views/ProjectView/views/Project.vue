80a75161080d7418d606b09981ef0a0d0fa80080
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-27 13:30:33 +0330

    add: live tracking feature

--- src/views/ProjectView/views/Project.vue
+++ src/views/ProjectView/views/Project.vue
@@ -85,7 +85,7 @@ import { parser, enumUtils } from '@/utils';
                         <a v-on:click="download(item.id)" style="margin-right: 5px;" class="btn btn-sm btn-light" download>
                             view log file
                         </a>
-                        <a v-on:click="rerun(item.id)" class="btn btn-sm btn-dark">
+                        <a v-on:click="rerun(item.id)" style="margin-right: 5px;" class="btn btn-sm btn-dark">
                             rerun the test
                         </a>
                     </div>
@@ -112,13 +112,20 @@ import { parser, enumUtils } from '@/utils';
                 </svg>
                 reload
             </button>
+            <button class="btn btn-secondary mt-3" style="margin-inline-start: 5px;" v-on:click="liveTrack">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi me-2 bi-chat-text" viewBox="0 0 16 16">
+                    <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894m-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                    <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5"/>
+                </svg>
+                live tracking
+            </button>
         </div>
     </div>
 </template>
 
 <script>
 import { useRoute } from 'vue-router';
-import { projectsApi } from '@/api';
+import { projectsApi, liveApi } from '@/api';
 import { fetchWrapper } from '@/helpers';
 import { useAlertStore } from '@/stores';
 
@@ -155,6 +162,25 @@ export default {
             if (file != null) {
                 window.open(file, '_blank').focus();
             }
+        },
+        async liveTrack() {
+            let rsp = await liveApi.get(this.project_id, 0);
+            let newWin = window.open("about:blank", "live-tracking", "width=800,height=300");
+
+            newWin.document.write("<h2>Live Tracking</h2>");
+
+            if (rsp.length == 0) {
+                newWin.document.write("No events!");
+
+                return
+            }
+
+            rsp.forEach((el) => {
+                let message = `${el['time']}: [EID-${el['id']}][T: ${el['type']}][Service: ${el['service']}] ${el['description']}`
+
+                newWin.document.write(message);
+                newWin.document.write("<br />");
+            })
         },
         async execute() {
             if (!this.executealbe) {
