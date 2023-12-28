d7b8d27988512117694829f2ba92e641fe1d3562
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-28 19:03:30 +0330

    update: general navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,10 +9,13 @@ const authStore = useAuthStore();
   <div class="d-flex flex-row align-items-center justify-content-between flex-shrink-0 p-3 text-white bg-dark" style="height: 100vh;">
     <RouterLink to="/" class="d-flex align-items-center mb-3 mb-md-0 text-white text-decoration-none">
       <span class="fs-4 text-danger">
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi me-2 bi-fire" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi me-2 bi-fire" viewBox="0 0 16 16">
           <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16m0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15"/>
         </svg>
         PTaaS Dashboard
+        <span class="badge bg-danger">
+          {{ version }}
+        </span>
       </span>
     </RouterLink>
     <hr>
@@ -72,3 +75,16 @@ const authStore = useAuthStore();
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      version: ""
+    }
+  },
+  mounted() {
+    this.version = import.meta.env.VITE_VERSION;
+  }
+}
+</script>
